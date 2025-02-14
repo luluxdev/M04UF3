32866dbf50098d7b806171cc7bb1c7bf1ada8dde
Lucia Puga <lucia.puga@enti.cat>
2025-02-14 11:30:22 +0100

    Errores

--- ejemplo/src/App.js
+++ ejemplo/src/App.js
@@ -8,7 +8,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Holi, esta es mi aplicación React.
-          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -16,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Enlace!
         </a>
       </header>
     </div>
