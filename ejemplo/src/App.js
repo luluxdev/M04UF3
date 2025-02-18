80b98c6f8c7990e81f509af38f8f732295198548
☆ luluxdev ☆ <lucia.puga@enti.cat>
2025-02-18 17:03:17 +0100

    Cambios

--- ejemplo/src/App.js
+++ ejemplo/src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-				<p>
-        Holaaaaaa
-				Edit <code>src/App.js</code> and save to reload.
+        <p>
+					Holaaaaaaaaaaaaaa
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
