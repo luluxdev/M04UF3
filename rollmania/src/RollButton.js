ef698ee262cec0d42a33345d88172d98e9fdc15b
☆ luluxdev ☆ <lucia.puga@enti.cat>
2025-02-26 22:54:49 +0100

    Cuenta atrás y roll

--- rollmania/src/RollButton.js
+++ rollmania/src/RollButton.js
@@ -1,11 +1,24 @@
+import { useState, useEffect } from 'react';
 import './RollButton.css';
 
 function RollButton(props) {
+	// Estado para la cuenta regresiva, inicia en 5
+  const [count, setCount] = useState(5);
+
+  useEffect(() => {
+    if (count > 0) {
+		// Se ejecuta un setTimeout que reduce count en 1 cada segundo
+      setTimeout(() => setCount(count - 1), 1000);
+    }
+	// El efecto se ejecuta cada vez que count cambia
+  }, [count]);
+
   return (
-    <button className="RollButton" onClick={props.roll_func} >
-			Roll!!!
+    <button className="RollButton" onClick={props.roll_func}>
+      {count > 0 ? count : 'Roll!!!'}
     </button>
   );
 }
 
 export default RollButton;
+
